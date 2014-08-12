ec1a4f43fbc05fe6acaed77d38bc3d6339bd9ee1
yonyy <yonatan.perez.0213@gmail.com>
2014-08-12 15:01:36 -0700

    Added functionality and fixed bugs
    Features:
    15 max tries
    Displays the length of the word
    Automatically generates a new word when won or lost

--- hangman.c
+++ hangman.c
@@ -3,80 +3,181 @@
 #include <time.h>
 #include <string.h>
 
-char * selectWord();
-void printWelcomeMessages(int wordLength);
-char printPrompt();
+#define TRUE 1
+#define FALSE 0
+#define DASH '_'
 
-struct letter 
+typedef struct Letter 
 {
-	char character;
-	int beenGuessed;
-};
+	int gameOver;  /* If the whole word has been guessed */
+  int numRight;   /* Number of letters correctly guessed */
+	char randomWord[80];
+
+}Letter;
+
+int selectWord(char letters []);
+int compareChar(char input, char * originalWord, char guesses []);
+int gameOver(int numRight, int wordLength, char letters []);
+void printWelcomeMessages(int wordLength);
+void printChar(char guesses [], int length);
+void newGame(Letter newGame, char letters []);
 
 int main(int argc, char * argv[]) 
 {
-	char * word;
-	char * tempPtr;
-	int wordLength = 0;
-	word = selectWord();
-	tempPtr = word;
 
-	while( *tempPtr != 0 )
-	{
-		wordLength++;
-		tempPtr++;
-	}
+  Letter letter = {FALSE, 0};
+  char guesses[80];
+  int wordLength;
+  int dashes;
+  int numGuesses;
+  int maxTries;
+  
+  while(letter.gameOver != TRUE)
+  {
+    char * word;
+    char * ptr;
+   	wordLength = 0;
+    dashes = 0;
+    numGuesses = 0;
+    maxTries = 15;
+	  wordLength = selectWord(letter.randomWord);        /* Selecting random word */
+	  word = letter.randomWord;                          /* Assigning word  to the randomWord */
+    ptr = strchr(word,'\n');                           /* Getting rid of the new line */ 
+    *ptr = '\0';
 
-	printWelcomeMessages( wordLength );
-
-	char input;
-	while( input != -1 )
-	{
-		printf( "Guess a letter: ");
-		input = getchar();
-	}
+    for (dashes = 0; dashes < wordLength; dashes ++)  /* For loop to print out the dashes */
+    {
+      guesses[dashes] = DASH;
+    }
+    
+    printWelcomeMessages( wordLength );
 	
+    char input;
+	
+    while(letter.gameOver != TRUE)
+	  {
+      printf("Number of guesses: %i\n",numGuesses);
+      printf("Number of tries left: %i\n",(maxTries - numGuesses));
+		  printf( "Guess a letter: ");
+		  input = getchar();
+      numGuesses++;
 
+      if (input == '\n' || input == ' ')
+      {
+        input = getchar();
+      }
+      if (compareChar(input,word,guesses))
+      {
+        printf("\n\nYou guessed '%c' correctly\n\n",input);
+      }
+      else
+      {
+        printf("\n\nSorry, '%c' is not a letter in the word\n\n",input);
+      }
+
+      printChar(guesses,wordLength);
+      if (!gameOver(letter.numRight,wordLength,guesses))
+      {
+        printf("Congratulations! You have correctly guessed '%s'!\n\n",guesses);
+        break;
+      }
+      if (numGuesses == maxTries)
+      {
+        printf("GAME OVER!\nYou have failed to guess '%s'\n\n",word);
+        break;
+      }
+      
+	  }
+
+    newGame(letter,guesses);
+  }
 	return 0;
+}
+
+void newGame(Letter newGame, char letters [])
+{
+
+  memset(&newGame,0,sizeof(Letter));
+  memset(letters,' ',80);
+  newGame.numRight = 0;
+  newGame.gameOver = FALSE;
+  printf("Starting a new game...\n");
+}
+int gameOver(int numRight, int wordLength, char letters [])
+{
+  int index;
+  numRight = 0;
+  for (index = 0; index < wordLength; index++)
+  {
+    if (letters[index] != DASH)
+      numRight++;
+  }
+  
+  return (wordLength - numRight);
+}
+void printChar(char guesses [], int length)
+{
+  int i = 0;
+  for ( i = 0; i < length; printf("%c ",guesses[i]), i++);
+  puts("\n");
+}
+int compareChar(char input, char * originalWord, char guesses [])
+{
+  char * tempPtr = originalWord;
+  int index = 0;
+  int guess = FALSE;
+  while (*tempPtr != '\n')
+  {
+    if (input == *tempPtr)
+    {
+      guess = TRUE;
+      guesses[index] = *tempPtr;
+    }
+    tempPtr++;
+    index++;
+  }
+  return guess;
 }
 
 void printWelcomeMessages(int wordLength)
 {
-	system("clear");
+  int dashes;
 	printf("WELCOME TO HANGMAN\n");
 	printf("\nSelecting a random word.");
 	printf("\nYour word is %d characters long...Good Luck!", wordLength);
 	puts("\n");
+
+  for (dashes = 0; dashes < wordLength; dashes++)
+  {
+    printf("_ ");
+  }
+  puts("\n");
 }
 
-char printPrompt()
-{
-	char input;
-	printf("\nGuess a letter: ");
-	scanf( "%c", &input );
-	return input;
-}
-
-char * selectWord()
+int selectWord( char letters[])
 {
 	FILE * fp = fopen("./words", "r" );
-	char randomWord[80];
+  char c;
+  int index = 0;
 	int lineCount = 235886; //number of words in local dictionary
 	srand(time(NULL));
 	int randomLine = rand() % lineCount; //random line
-	int index = 0;
 	while( index < randomLine )
 	{
 		index++;
-		fgets( randomWord, 80, fp );
+		(void)fgets( letters, 80, fp );
 	}
-
-
-	fclose(fp);
-
-	char * word = randomWord;
-	return word;
-
+  index = 0;
+  do {
+    c = fgetc(fp);
+    letters[index] = c;
+    index++;
+    } while (c != '\n');
+	
+  fclose(fp);
+  return (index - 1);
 }
+
+
 
 
